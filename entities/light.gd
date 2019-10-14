e2d036ccbbffec115978399ca3bf7ac807beb584
Herman Chau <beijingherman@gmail.com>
2019-10-13 22:07:30 -0700

    Good progress, figured out how to make things fast enough, implemented first pass at physics in shadow.

--- entities/light.gd
+++ entities/light.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Polygon2D
 
 onready var rc = get_node('raycast')
 var UPDATE_INTERVAL = 1000/60
@@ -10,8 +10,8 @@ func _ready():
 func _process(delta):
 	nextUpdate -= delta*1000
 	if nextUpdate <= 0:
-		nextUpdate = UPDATE_INTERVAL
 		get_parent().position = get_viewport().get_mouse_position()
+		nextUpdate = UPDATE_INTERVAL
 		var offset = get_parent().position
 		var maps = get_tree().get_nodes_in_group('collision_map')
 		var points = []
@@ -23,20 +23,14 @@ func _process(delta):
 				var cell_offset = map.map_to_world(pos)
 				if occluder == null:
 					continue
-				var isVisible = false
-				var occluderPoints = []
 				for p in occluder.polygon:
-					for radOffset in [0,-0.00001, 0.00001]:
-						var point = (p+cell_offset - offset).rotated(radOffset)
+					for radOffset in [0,-0.0001, 0.0001]:
+						var point = p+cell_offset - offset
+						point = point.rotated(radOffset).normalized()*10000
 						rc.cast_to = point
 						rc.force_raycast_update()
 						if rc.is_colliding():
-							occluderPoints.append(rc.get_collision_point() - offset)
-							if (rc.get_collision_point() - offset - point).length() < 0.1:
-								isVisible = true
-				if isVisible:
-					for p in occluderPoints:
-						points.append(p)
+							points.append(rc.get_collision_point() - offset)
 		points.sort_custom(self, "sortByAngle")
 		var dedupedPoints = []
 		var currentPoint = null
@@ -45,10 +39,8 @@ func _process(delta):
 				continue
 			dedupedPoints.append(p)
 			currentPoint = p
-		print(dedupedPoints.size())
 		points = PoolVector2Array(dedupedPoints)
-		get_node('visiblePolygon').polygon = points
-		get_node('collisionPolygon').polygon = points
+		polygon = points
 
 func sortByAngle(a,b):
 	return a.angle() < b.angle()
--- entities/player.gd
+++ entities/player.gd
@@ -1,18 +1,24 @@
 extends KinematicBody2D
 
-const MAX_FALL = 180
-const MAX_RUN = 120
-const RUN_ACCEL = 1000
-const GRAVITY = 500
+const MAX_FALL = 500
+const MAX_RUN = 200
+const RUN_ACCEL = 2000
+const RUN_REDUCE = 750
+const MAX_SWIM = 120
+const SWIM_ACCEL = 500
+const SWIM_FRICTION = 0.95
+const SWIM_DASH_ACCEL = 500
+const GRAVITY = 900
 const HALF_GRAVITY_THRESHOLD = 10
 const JUMP_GRACE_TIME = 0.1
 const JUMP_H_BOOST = 40
-const JUMP_SPEED = -205
+const JUMP_SPEED = -305
 const JUMP_INPUT_BUFFER = 0.1
 
 var speed = Vector2(0,0)
 var jumpGraceTime = 0
 var inputMoveX = 0
+var inputMoveY
 var inputJump = 0 
 
 func approach(curr, bound, delta):
@@ -23,14 +29,8 @@ func _ready():
 	set_process(true)
 
 func updateInput(delta):
-	if Input.is_action_just_pressed('left'):
-		inputMoveX = -1
-	elif Input.is_action_just_released('left'):
-		inputMoveX = 1 if Input.is_action_pressed('right') else 0
-	if Input.is_action_just_pressed('right'):
-		inputMoveX = 1
-	elif Input.is_action_just_released('right'):
-		inputMoveX = -1 if Input.is_action_pressed('left') else 0
+	inputMoveX = (-1 if Input.is_action_pressed('left') else 0) + (1 if Input.is_action_pressed('right') else 0)
+	inputMoveY = (-1 if Input.is_action_pressed('up') else 0) + (1 if Input.is_action_pressed('down') else 0)
 	
 	if Input.is_action_just_pressed('jump'):
 		inputJump = JUMP_INPUT_BUFFER
@@ -39,24 +39,51 @@ func updateInput(delta):
 
 func _process(delta):
 	updateInput(delta)
+	var isInLight = inLight()
 
-	if is_on_floor():
-		jumpGraceTime = JUMP_GRACE_TIME
+	# Regular platforming
+	if isInLight:
+		if is_on_floor():
+			jumpGraceTime = JUMP_GRACE_TIME
+		else:
+			jumpGraceTime -= delta
+		
+		# Horizontal movement
+		var maxX = MAX_RUN*inputMoveX
+		if abs(speed.x) > abs(maxX) and sign(speed.x) != -inputMoveX:
+			speed.x = approach(speed.x, maxX, RUN_REDUCE*delta)
+		else:
+			speed.x = approach(speed.x, maxX, RUN_ACCEL*delta)
+		
+		# Gravity
+		if not is_on_floor():
+			var gravityMult = 0.5 if abs(speed.y) < HALF_GRAVITY_THRESHOLD and Input.is_action_pressed('jump') else 1
+			speed.y = approach(speed.y, MAX_FALL, GRAVITY*delta*gravityMult)
+		else:
+			speed.y = 0
+		
+		# Jumping
+		if inputJump > 0 and jumpGraceTime > 0:
+			jump()
 	else:
-		jumpGraceTime -= delta
-
-	# Horizontal movement
-	speed.x = approach(speed.x, MAX_RUN*inputMoveX, RUN_ACCEL*delta)
-
-	# Gravity
-	print(inLight())
-	if not is_on_floor():
-		var gravityMult = 0.5 if abs(speed.y) < HALF_GRAVITY_THRESHOLD and Input.is_action_pressed('jump') else 1
-		speed.y = approach(speed.y, MAX_FALL, GRAVITY*delta*gravityMult)
-
-	# Jumping
-	if inputJump > 0 and jumpGraceTime > 0:
-		jump()
+		var maxX = MAX_SWIM*inputMoveX/SWIM_FRICTION
+		var maxY = MAX_SWIM*inputMoveY/SWIM_FRICTION
+		if inputMoveX != 0:
+			speed.x = approach(speed.x, maxX, SWIM_ACCEL*delta)*SWIM_FRICTION
+		else:
+			speed.x *= SWIM_FRICTION
+		if inputMoveY != 0:
+			speed.y = approach(speed.y, maxY, SWIM_ACCEL*delta)*SWIM_FRICTION
+		else:
+			speed.y *= SWIM_FRICTION
+		if inputJump > 0:
+			if inputMoveX == 0 and inputMoveY == 0:
+				speed.x = SWIM_DASH_ACCEL
+				speed.y = 0
+			else:
+				speed.x = inputMoveX*SWIM_DASH_ACCEL
+				speed.y = inputMoveY*SWIM_DASH_ACCEL
+			inputJump = 0
 
 	move_and_slide(speed, Vector2(0, -1))
 
@@ -68,6 +95,14 @@ func jump():
 
 func inLight():
 	for light in get_tree().get_nodes_in_group('light'):
-		if light.get_node('Area2D').overlaps_area(get_node('Area2D')):
-			return true
+		var lightPoints = light.get_node('visiblePolygon').polygon
+		var triangulatedPoints = Geometry.triangulate_polygon(lightPoints)
+		if triangulatedPoints.size() == 0:
+			continue
+		for i in range(triangulatedPoints.size()/3):
+			var a = lightPoints[triangulatedPoints[3*i]] + light.position
+			var b = lightPoints[triangulatedPoints[3*i+1]] + light.position
+			var c = lightPoints[triangulatedPoints[3*i+2]] + light.position
+			if Geometry.point_is_inside_triangle(position,a,b,c):
+				return true
 	return false
